38bccaf73ec41113fc9d7c3e4b1e10471d2afc77
Erencan Duymaz <erencanduymaz@gmail.com>
2017-07-27 15:16:16 +0300

    Geared WECS oluşturmaya çalışıyorum DD-GD karşılaştırması için

---
+++ Other Material/WECS/WCTS_Geared.m
@@ -0,0 +1,17 @@
+% Support Time 
+
+sup = 15 ; 
+% Wind Turbine Parameters 
+R = 61.44; % m
+p = 1.225; % air density kg/m3,
+wspeed = 11.4 ; %m/s
+Jtur= 35444067; %kgm2
+
+%Gear Ratio
+gear = 97 ;
+
+
+%Generator Parameters 
+Jgen = 534.116; %kgm2
+pgen = 4; 
+winit = -147.4; %initial generator speed
