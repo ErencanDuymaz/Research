32beae6be77d06dc1ff56d755c0f897bef9590a4
Erencan Duymaz <erencan.duymaz@metu.edu.tr>
2017-10-25 17:58:11 +0300

    Updated!

--- Other Material/SIMULINK MODELS/Tobecontinued/enerjisa.m
+++ Other Material/SIMULINK MODELS/Tobecontinued/enerjisa.m
@@ -1,45 +1,47 @@
 clear all
 clc
+
 % Support Time 
 sup = 15 ; 
 
 %Sample Time
 Ts = 10e-6;
-tur
+
 % Initiation
-winit = -150;
+winit = -100;
 
 % Wind Turbine Parameters
 Prated =2750000;
 R = 51.5; % m
 p = 1.225; % air density kg/m3,
-windspeed = 10.4  ; %m/s
+windspeed = 10.4; %m/s
 windrated = 13; %m/s
 Jtur= 35444067; %kgm2
-Betainit = 0;    %Pitch Angle
+
    
 %Gear Ratio
 gear = 97 ;
 
-
 %Generator Parameters 
+Ld=2.48e-4;
+Lq=2.92e-4;
+Rs=0.7305e-3;
 Jgen = 534.116; %kgm2
 pgen = 4; 
-winit = -147.4; %initial generator speed
-% kt = 4.863e-3;
+
+
 fluxlinkage=5.6; % V.s
-Ld=2.31e-3;
-Lq=1.21e-3;
+Ld=2.48e-4;
+Lq=2.92e-4;
 Rs=0.7305e-3;
 
 
 
-
+%Total Inertia Calculation
 Jtotal= Jgen+Jtur/(gear^2);
 
 
 %DC Bus Settings 
-
 Vdcset = 3000; %% to be updated
 
 
