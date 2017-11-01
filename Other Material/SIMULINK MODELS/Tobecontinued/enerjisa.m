422b6de8db86c0d46db61e4522885131d0a74187
Erencan Duymaz <erencan.duymaz@metu.edu.tr>
2017-11-01 16:43:16 +0300

    Trigerred subsystem is included in the system for better imitating the driver characteristics

--- Other Material/SIMULINK MODELS/Tobecontinued/enerjisa.m
+++ Other Material/SIMULINK MODELS/Tobecontinued/enerjisa.m
@@ -6,9 +6,10 @@ sup = 15 ;
 
 %Sample Time
 Ts = 10e-6;
-
+drivefreq = 5e3; % The frequency of the drive
 % Initiation
 winit = -100;
+
 
 % Wind Turbine Parameters
 Prated =2750000;
@@ -16,7 +17,7 @@ R = 51.5; % m
 p = 1.225; % air density kg/m3,
 windspeed = 10.4; %m/s
 windrated = 13; %m/s
-Jtur= 35444067; %kgm2
+Jtur= 3e6; %kgm2
 
    
 %Gear Ratio
@@ -25,8 +26,8 @@ gear = 97 ;
 %Generator Parameters 
 Ld=2.48e-4;
 Lq=2.92e-4;
-Rs=0.7305e-3;
-Jgen = 534.116; %kgm2
+Rs=0.7305e-4;
+Jgen = 200; %kgm2
 pgen = 4; 
 
 
