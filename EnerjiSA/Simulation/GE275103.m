34f8170d2e6abefb100d3849a80e2c704e8f5470
Erencan Duymaz <erencan.duymaz@metu.edu.tr>
2018-03-13 15:13:42 +0300

    Results are obtained.

---
+++ EnerjiSA/Simulation/GE275103.m
@@ -0,0 +1,50 @@
+clear all
+clc
+
+%Sample Time
+Ts = 5E-6;
+drivefreq = 30e3; % The frequency of the drive
+
+%Initiation
+winit=-185;
+
+% Wind Turbine Parameters
+Prated =2750000;
+
+R = 51.5; % m
+p = 1.225; % air density kg/m3,
+windspeed = 10; %m/s
+windrated = 13; %m/s
+Jtur= 13e6; %kgm2   %Updated
+
+%Gear Ratio
+gear = 117.4 ;
+
+%Generator Parameters 
+Jgen = 115; %kgm2
+fluxlinkage=4;
+pgen = 2;  %Pole Pair
+
+Ld=2.48e-4;
+Lq=2.92e-4;
+Rs=1e-10;%0.7305e-4
+Ldf=1e-3;
+Lqf=1e-3;
+
+Jtotal= Jgen+Jtur/(gear^2);
+
+%DC Bus Settings
+Vdcset = 2500; %updated! bu asl?nda böyle de?ildir ama for the time being
+Cdc=27e-3;     %updated!
+
+%Filter 
+Lfilter=2e-4;
+
+%Inertia Support
+H=5.6; %seconds
+fnom=50;
+suptime=10;
+
+
+
+
