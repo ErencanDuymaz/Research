0f17be3accf088e3a5342297985ab68db665be8a
Erencan Duymaz <erencan.duymaz@metu.edu.tr>
2018-03-20 16:12:30 +0300

    World Renewable Energy Capacities and Production at the end of 2016. Reproducable Figures

---
+++ Thesis/Matlab Codes/World Renewable Capacity and Production/renewableproduction.m
@@ -0,0 +1,26 @@
+% countries = categorical({'China','USA','Brazil','Germany','Canada','India','Japan', 'Russian Fed', 'Italy', 'Spain','France','Turkey','UK','Norway'});
+% capacities = [545.916,214.766,122.951,104.704,97.517,90.748,75.125,51.35,51.07,47.954,44.666,34.467,33.516,32.744,27.877];
+% bar(countries,capacities)
+
+clear all
+clc
+filename = 'World Renewable Capacity.xlsx';
+sheet = 3;
+xlRange1 = 'B2:D16';
+countries = [];
+
+[num,txt] = xlsread(filename,sheet,xlRange1);
+c = categorical(txt);
+c = c';
+num2=num(:,2);
+fig = bar(num2,0.5)
+xticklabels(txt) 
+xlabel('Countries')
+ylabel('TWh')
+ax = gca;
+ax.XGrid = 'off';
+set(gca,'FontSize',12);
+% grid minor
+ax.YGrid = 'on';
+
+saveas(gcf,'renewableproduction','epsc')
