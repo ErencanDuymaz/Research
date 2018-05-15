6a261ce93fb35be7b84650e69cbc9ecf53b67d6a
Erencan Duymaz <erencan.duymaz@metu.edu.tr>
2018-05-15 12:12:46 +0300

    Wind Turbine block is created under mask. Two turbine operated in the same grid :)

--- Other Material/SIMULINK MODELS/GE2.75/GE275103.m
+++ Other Material/SIMULINK MODELS/GE2.75/GE275103.m
@@ -11,24 +11,17 @@ Prated =2750000;
 Srated=3.04e6;
 
 
-R = 51.5; % m
+% R = 51.5; % m
 p = 1.225; % air density kg/m3,
-windspeed = 10; %m/s
-windrated = 13; %m/s
+% windspeed = 9; %m/s
 Jtur= 13e6; %kgm2   %Updated
 wrated=(2*pi*1735)/60;
 wmax=2*pi*(1735/60);
 %Gear Ratio
-gear = 117.4;
+% gear = 117.4;
 
 %MPPT Speed
-%If the B=0
-wMPPT=(8.1*windspeed*gear)/R;
-if wMPPT>wmax
-    wMPPT=wmax;
-end
-%Initiation
-winit=-wMPPT-5;
+
 
 %Generator Parameters 
 Jgen = 115; %kgm2
@@ -36,16 +29,13 @@ fluxlinkage=4;
 pgen = 2;  %Pole Pair
 Tbase=Srated/((2*pi*fnom)/pgen);
 
-Ld=2.48e-4;
-Lq=2.92e-4;
-% Ld=20e-4;
-% Lq=21e-4;
+% Ld=2.48e-4;
+% Lq=2.92e-4;
+Ld=10e-4;
+Lq=10.5e-4;
 
 Rs=1e-10;%0.7305e-4
-Ldf=1e-3;
-Lqf=1e-3;
-
-Jtotal= Jgen+Jtur/(gear^2);
+% Jtotal= Jgen+Jtur/(gear^2);
 
 %DC Bus Settings
 Vdcset = 2500; %updated! bu asl?nda böyle de?ildir ama for the time being
@@ -53,15 +43,6 @@ Cdc=27e-3;     %updated!
 
 %Filter 
 Lfilter=2e-4;
-%  Lfilter=1e-3;
-
-%Inertia Support
-H=5; %seconds
-fnom=50;
-suptime=40;
-loadtrip=8;
-gendelay=1.5;
-supduration=2;
 startuptime=5;
 
 
