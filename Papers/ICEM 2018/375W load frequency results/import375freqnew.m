4566c521820e09471e612b77a6f4b7ebf6289941
Erencan Duymaz <erencan.duymaz@metu.edu.tr>
2018-07-10 16:32:23 +0300

    Mean Frequency and Standard Deviation for frequency results

--- Papers/ICEM 2018/375W load frequency results/import375freqnew.m
+++ Papers/ICEM 2018/375W load frequency results/import375freqnew.m
@@ -12,16 +12,3 @@ freqsim=xlsread('375W new.xlsx','G2:G6002');
 plot(time2,freqsim,'--k','LineWidth',2)
 
 
-xlabel('Time(s)')
-ylabel('Frequency (Hz)')
-ax = gca;
-ax.XGrid = 'on';
-set(gca,'FontSize',14);
-grid minor
-ax.YGrid = 'on';
-xlim([0 30])
-ylim([48 50.25])
-
-legend('Experimental Result','Simulation Result')
-
-saveas(gcf,'375freq.png')
