8b2e3176b955f2506f2571539e92e826261e4a46
Erencan Duymaz <erencan.duymaz@metu.edu.tr>
2018-11-05 01:23:19 +0300

    Thesis is updated. Inertial support limits are evaluated.

--- Other Material/Wind Turbine Codes/torque_variation.m
+++ Other Material/Wind Turbine Codes/torque_variation.m
@@ -7,5 +7,12 @@ Cpptemp=Cptemp/i;
 Cpp= [ Cpp Cpptemp];
 tip= [ tip i ];
 end
-
-plot(tip,Cpp)
+a=max(Cpp);
+plot(tip,Cpp/a,'LineWidth',2)
+xlabel('Tip Speed Ratio')
+ylabel('Torque (pu)')
+ax = gca;
+ax.XGrid = 'on';
+set(gca,'FontSize',15);
+grid minor
+ylim([0 1.05])
