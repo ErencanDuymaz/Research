5de88d10ac3cec61bc324534f145c1ff2251f262
Erencan Duymaz <erencan.duymaz@metu.edu.tr>
2018-11-19 23:48:27 +0300

    chapter 3 is updated.

---
+++ Other Material/Wind Speed 8 Month/power_coefficient.m
@@ -0,0 +1,82 @@
+clear all
+clc
+filename = 'wind speeds.xlsx';
+sheet = 2;
+xlRange = 'A31:A201';
+
+[num,txt] = xlsread(filename,sheet,xlRange);
+windspeeds=num;
+
+clear vars filename sheet txt xlRange num sheet
+
+filename = 'wind speeds.xlsx';
+sheet = 2;
+xlRange = 'I31:I201';
+
+[num,txt] = xlsread(filename,sheet,xlRange);
+cp=num;
+
+clear vars filename sheet txt xlRange num sheet
+
+plot(windspeeds,cp,'k','LineWidth',2)
+
+xlabel('Wind Speeds (m/s)')
+ylabel('Power Coefficient')
+ax = gca;
+ax.XGrid = 'on';
+ax.YGrid = 'on';
+ax.XColor = 'k'; % Red
+ax.YColor = 'k'; % Blue
+grid minor
+xlim([3 20])
+ylim([0 0.5])
+title('')
+set(gca,'FontSize',12);
+% saveas(gcf,'cp_wind','pdf')
+
+%%
+clear all
+clc
+filename = 'wind speeds.xlsx';
+sheet = 2;
+xlRange = 'A31:A201';
+
+[num,txt] = xlsread(filename,sheet,xlRange);
+windspeeds=num;
+
+clear vars filename sheet txt xlRange num sheet
+
+filename = 'wind speeds.xlsx';
+sheet = 2;
+xlRange = 'J31:J201';
+
+[num,txt] = xlsread(filename,sheet,xlRange);
+cp=num;
+
+clear vars filename sheet txt xlRange num sheet
+
+% plot(windspeeds,cp)
+% xlabel('Wind Speeds (m/s)')
+% ylabel('Output Power (W)')
+% ax = gca;
+% ax.XGrid = 'on';
+% ax.YGrid = 'on';
+% grid minor
+% set(gca,'FontSize',20);
+
+plot(windspeeds,cp,'k','LineWidth',2)
+
+xlabel('Wind Speed (m/s)')
+ylabel('Output Power (W)')
+ax = gca;
+ax.XGrid = 'on';
+ax.YGrid = 'on';
+ax.XColor = 'k'; % Red
+ax.YColor = 'k'; % Blue
+grid minor
+xlim([3 20])
+ylim([0 3e6])
+title('')
+set(gca,'FontSize',12);
+
+% saveas(gcf,'powerdata','pdf')
