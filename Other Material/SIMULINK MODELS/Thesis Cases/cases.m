fb294b44bb66661d20103f473e35c3695c048fe6
Erencan Duymaz <erencan.duymaz@metu.edu.tr>
2018-12-04 23:59:44 +0300

    Thesis is updated.

---
+++ Other Material/SIMULINK MODELS/Thesis Cases/cases.m
@@ -0,0 +1,15 @@
+clear all
+clc
+Ts=1e-5;
+
+Sbase=100e6;
+Vbase=230e3; 
+Ibase=Sbase/(sqrt(3)*Vbase);
+
+Xbase=(Vbase^2)/Sbase;
+Ybase=1/Xbase; 
+
+Lbase=Xbase/(100*pi);
+Cbase=1/(100*pi*Xbase);
+startup=2; 
+loadconnection=10;
